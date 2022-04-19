205cd397b0ed3d436a889ee9ffa8d528e60e4dbe
billyalx <billyalexndr@gmail.com>
2022-04-19 13:39:22 +0700

    Construction Puzzle

---
+++ main.cpp
@@ -0,0 +1,13 @@
+﻿#include "Puzzle.h"
+
+using namespace std;
+
+int main()
+{
+	srand(unsigned(time(0)));
+
+	// randomize the puzzle
+	Puzzle puzzle(3, 4, "logoUPNVJ.png", 1000, 500);
+
+	puzzle.run();
+}
